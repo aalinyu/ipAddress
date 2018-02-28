c6146fb1bb9b20768e318e81aabc14d449fb3ef9
shuijing <shuijing@yiji.com>
2018-02-28 22:04:13 +0800

    修改文档

--- README.md
+++ README.md
@@ -1,29 +1,32 @@
 ## 1. 组件介绍
 
-此组件提供ip查询地理位置功能。
+此组件提供ip查询地理位置功能
 
 ## 2. 配置
 
 1) 增加组件依赖
 
+    ```
     <dependency>
       <groupId>org.aalin.common</groupId>
-      <artifactId>ip.address</artifactId>
+      <artifactId>ipaddress</artifactId>
       <version>1.0</version>
     </dependency>
+    ```
 
 ## 3.使用
-  **[接口说明](http://gitlab.yiji/shuijing/yjf-common-iputil/blob/master/src/main/java/com/yjf/common/ip/IpLookup.java)**
 
+  1. [接口详情](https://gitee.com/aalinyu/ipaddress/blob/master/src/main/java/org/aalin/common/ip/IpLookup.java)**
+  2. 使用方法
    `IpLookupResult result = DefaultIpLookup.getInstance().lookup("11.23.56.34",false);`
    
 ## 4.注意
-   查询顺序
-    1、新浪接口 超时时间100ms
-    2、百度接口 超时时间100ms
-    3、本地库 <10ms
-
-    如果不能访问外网，可只查询本地库，isOnlyLookupLocal=ture
+1. 新浪接口 超时时间100ms
+2. 百度接口 超时时间100ms
+3. 本地库 查询时间<2ms
+4. 如果不能访问外网，可只查询本地库，isOnlyLookupLocal=ture
+    
+    ```
     /**
     	 * ip地址本地库查询
     	 * @param ip
@@ -42,8 +45,7 @@
     	 *          country、city、district返回值有可能为空
     	 * */
     	IpLookupResult lookup(String ip,boolean isOnlyLookupLocal);
-
-
+    ```
    
    
    
--- src/test/java/org/aalin/common/ip/IPTest.java
+++ src/test/java/org/aalin/common/ip/IPTest.java
@@ -8,23 +8,28 @@ import org.aalin.common.ip.model.IpLookupResult;
 
 import static org.aalin.common.ip.executor.impl.LocalRepositoryLookupExecutor.randomIp;
 
-/**
- * @author shuijing
- */
+/** @author shuijing */
 public class IPTest {
-    public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            IpLookupResult result = DefaultIpLookup.getInstance().lookup(randomIp(), false);
-            System.out.println(result);
-        }
-        //testBaiduInterface();
-    }
+  public static void main(String[] args) {
+    //    for (int i = 0; i < 10; i++) {
+    //      IpLookupResult result = DefaultIpLookup.getInstance().lookup(randomIp(), false);
+    //      System.out.println(result);
+    //    }
+    // testBaiduInterface();
+    testLocalInterface();
+  }
 
-    private static void testBaiduInterface(){
-        IpLookupExecutor executor = IpLookupProvider.getInstance().get(IpLookupEnum.BAIDU);
-        IpAddress lookup = executor.lookup(randomIp());
-        System.out.printf(lookup.toString());
-    }
+  private static void testBaiduInterface() {
+    IpLookupExecutor executor = IpLookupProvider.getInstance().get(IpLookupEnum.BAIDU);
+    IpAddress lookup = executor.lookup(randomIp());
+    System.out.printf(lookup.toString());
+  }
 
-
+  private static void testLocalInterface() {
+    IpLookupExecutor executor = IpLookupProvider.getInstance().get(IpLookupEnum.LOCAL);
+    long startTime = System.currentTimeMillis();
+    IpAddress lookup = executor.lookup(randomIp());
+    System.out.println("花费时间： " + (System.currentTimeMillis() - startTime)+"ms");
+    System.out.println(lookup.toString());
+  }
 }
